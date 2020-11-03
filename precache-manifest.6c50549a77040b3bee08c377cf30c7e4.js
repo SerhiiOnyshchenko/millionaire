b4354e22ae131f2b69cb80decbea3a65eac308f5
SerhiiOnyshchenko <vkhellboy1992@gmail.com>
2020-11-03 17:42:31 +0200

    Updates

---
+++ precache-manifest.6c50549a77040b3bee08c377cf30c7e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4f097b8a277fc5a405c646ba399e8d",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "8986611f2e82b9305f98",
+    "url": "/millionaire/static/css/main.4076eeb8.chunk.css"
+  },
+  {
+    "revision": "04687879d131bae4b548",
+    "url": "/millionaire/static/js/2.a2e584fb.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/millionaire/static/js/2.a2e584fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8986611f2e82b9305f98",
+    "url": "/millionaire/static/js/main.958a8d70.chunk.js"
+  },
+  {
+    "revision": "055faf80f34c618714c3",
+    "url": "/millionaire/static/js/runtime-main.2acf6893.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/millionaire/static/media/hand.f8c75481.png"
+  }
+]);
